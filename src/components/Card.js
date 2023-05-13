831a970829c2e14adf863e0606ad6777fc94b656
Danila <danila10danila@mail.ru>
2023-05-13 10:56:08 +0300

    styling changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,15 @@ const Card = ({ plant }) => {
         style={{backgroundImage: `url("/images/${plant.name}.jpg")`}}
         >
             <div className='title'>{plant.name}</div>
-            <div className='price'>{plant.price}$</div>
+            <div className='flexboxEnd'>
+                <div className='price'>{plant.price}$</div>
+                <div className='quantity'>
+                    <button>-</button>
+                    <input type="number"></input>
+                    <button>+</button>
+                </div>
+            </div>
+            
         </div>
         
     )
