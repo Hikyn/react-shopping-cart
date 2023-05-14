d9517bf7391c50d46ce5fda2cd379f5b786719e9
Danila <danila10danila@mail.ru>
2023-05-14 11:57:31 +0300

    feat: tests for Shop

---
+++ src/components/__tests__/Shop.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";  // optional
+import Shop from "../Shop";
+import { MemoryRouter } from "react-router-dom";
+
+describe("Shop component", () => {
+    it("Creates 10 cards", () => {
+        render(<MemoryRouter><Shop /></MemoryRouter>);
+        expect(screen.getAllByRole('article').length).toEqual(10);
+    });
+
+    it("Creates NavBar", () => {
+        render(<MemoryRouter><Shop /></MemoryRouter>);
+        expect(screen.getByRole('banner')).toBeDefined();
+    });
+
+    it("Creates OrderTotal", () => {
+        render(<MemoryRouter><Shop /></MemoryRouter>);
+        expect(screen.getByRole('footer')).toBeDefined();
+    });
+});
