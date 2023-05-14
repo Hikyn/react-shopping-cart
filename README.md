4eef6fb1ab9c91dd5be163aa5ee2b0396e135f45
Danila <danila10danila@mail.ru>
2023-05-14 11:17:52 +0300

    feat: card tests

--- README.md
+++ README.md
@@ -1,70 +1,47 @@
-# Getting Started with Create React App
+# Shopping cart project
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Encountered problems:
+### Tests:
+#### userEvent.setup()
+Wasn't able to use ```userEvent.setup()```, 
+figured out that ```npm create-react-app``` uses 
+```"@testing-library/user-event": "^13.5.0"```, but
+.setup() was introduced in 14.0.0. 
 
-## Available Scripts
+Solution: update dependency.
+```bash
+npm outdated
+npm install @testing-library/user-event@latest
+```
 
-In the project directory, you can run:
+#### Proper click testing
+We need to make sure that everything that takes time (rendering, user events, data fetching)
+is completed before test assertions are run.
 
-### `npm start`
+render() automatically wraps content in act() if we use React Testing Library, but
+user.click needed to be wrapped manually.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+```js
+await act(async() => {
+    await user.click(button);
+})
+```
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+#### Wrapping proper action
+If we use act() on for loop, it will not work.
+```js
+await act(async () => {
+    for(let i = 0; i < 5; i += 1) {
+        await user.click(button);
+    }
+})
+```
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Correct usage will be:
+```js
+for(let i = 0; i < 5; i += 1) {
+    await act(async () => {
+        await user.click(button);
+    })
+}
+```
---
+++ src/components/__tests__/Card.test.js
@@ -0,0 +1,235 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";  // optional
+import userEvent from "@testing-library/user-event";
+import Card from "../Card";
+import { plantFactory } from "../../plantFactory";
+import { shallow } from 'enzyme';
+import { act } from "react-dom/test-utils";
+
+describe("Card component", () => {
+    it("renders correct title", () => {
+        const plant = plantFactory('Appletini', 25);
+        const addPlantToTotal = jest.fn();
+        const decreasePlantFromTotal = jest.fn();
+        render(<Card 
+            key={plant.name} 
+            plant={plant}
+            addPlant={addPlantToTotal} 
+            decreasePlant={decreasePlantFromTotal}
+        />);
+        expect(screen.getByRole("heading", { level: 1 }).textContent).toMatch(/Appletini/i);
+    });
+
+    it("renders correct price", () => {
+        const plant = plantFactory('Appletini', 25);
+        const addPlantToTotal = jest.fn();
+        const decreasePlantFromTotal = jest.fn();
+        render(<Card 
+            key={plant.name} 
+            plant={plant}
+            addPlant={addPlantToTotal} 
+            decreasePlant={decreasePlantFromTotal}
+        />);
+        expect(screen.getByRole("heading", { level: 2 }).textContent).toMatch(/25/i);
+    });
+
+    it("renders default quantity", () => {
+        const plant = plantFactory('Appletini', 25);
+        const addPlantToTotal = jest.fn();
+        const decreasePlantFromTotal = jest.fn();
+        render(<Card 
+            key={plant.name} 
+            plant={plant}
+            addPlant={addPlantToTotal} 
+            decreasePlant={decreasePlantFromTotal}
+        />);
+        expect(screen.getByRole("spinbutton").value).toMatch(/0/i);
+    });
+
+    it("increases quantity", async () => {
+        const el = document.createElement("div");
+        const user = userEvent.setup();
+
+        const plant = plantFactory('Appletini', 25);
+
+        const addPlantToTotal = jest.fn();
+        const decreasePlantFromTotal = jest.fn();
+
+        render(<Card 
+            key={plant.name} 
+            plant={plant}
+            addPlant={addPlantToTotal} 
+            decreasePlant={decreasePlantFromTotal}
+        />);
+
+        const button = screen.getByRole("button", { name: "+" });
+
+        await act(async() => {
+            await user.click(button);
+        })
+        
+
+        expect(screen.getByRole("spinbutton").value).toMatch(/1/i);
+    });
+
+    it("increases quantity 5 times correctly", async () => {
+        // Ensure that no double click issue present
+        const user = userEvent.setup();
+
+        const plant = plantFactory('Appletini', 25);
+
+        const addPlantToTotal = jest.fn();
+        const decreasePlantFromTotal = jest.fn();
+
+        render(<Card 
+            key={plant.name} 
+            plant={plant}
+            addPlant={addPlantToTotal} 
+            decreasePlant={decreasePlantFromTotal}
+        />);
+
+        const button = screen.getByRole("button", { name: "+" });
+
+        for(let i = 0; i < 5; i += 1) {
+            await act(async () => {
+                await user.click(button);
+            })
+        }
+        
+        expect(screen.getByRole("spinbutton").value).toMatch(/5/i);
+    });
+
+    it("can't decrease quantity below zero", async () => {
+        // Ensure that no double click issue present
+        const user = userEvent.setup();
+
+        const plant = plantFactory('Appletini', 25);
+
+        const addPlantToTotal = jest.fn();
+        const decreasePlantFromTotal = jest.fn();
+        render(<Card 
+            key={plant.name} 
+            plant={plant}
+            addPlant={addPlantToTotal} 
+            decreasePlant={decreasePlantFromTotal}
+        />);
+        
+        const button = screen.getByRole("button", { name: "-" });
+
+        await act(async() => {
+            await user.click(button);
+        })
+        
+        expect(screen.getByRole("spinbutton").value).toMatch(/0/i);
+    });
+
+    it("can't decrease quantity from 1 to 0", async () => {
+        // Ensure that no double click issue present
+        const user = userEvent.setup();
+
+        const plant = plantFactory('Appletini', 25);
+
+        const addPlantToTotal = jest.fn();
+        const decreasePlantFromTotal = jest.fn();
+
+        render(<Card 
+            key={plant.name} 
+            plant={plant}
+            addPlant={addPlantToTotal} 
+            decreasePlant={decreasePlantFromTotal}
+        />);
+
+        expect(screen.getByRole("spinbutton").value).toMatch(/0/i);
+        const increaseButton = screen.getByRole("button", { name: "+" });
+
+        await act(async() => {
+            await user.click(increaseButton);
+        })
+        expect(screen.getByRole("spinbutton").value).toMatch(/1/i);
+
+        const decreaseButton = screen.getByRole("button", { name: "-" });
+
+        await act(async() => {
+            await user.click(decreaseButton);
+        })
+        
+        expect(screen.getByRole("spinbutton").value).toMatch(/0/i);
+    });
+
+    it("calls addPlantToTotal correct number of times", async () => {
+        const user = userEvent.setup();
+
+        const plant = plantFactory('Appletini', 25);
+
+        const addPlantToTotal = jest.fn();
+        const decreasePlantFromTotal = jest.fn();
+        
+        render(<Card 
+            key={plant.name} 
+            plant={plant}
+            addPlant={addPlantToTotal} 
+            decreasePlant={decreasePlantFromTotal}
+        />);
+
+        const increaseButton = screen.getByRole("button", { name: "+" });
+    
+        await act(async() => {
+            await user.click(increaseButton);
+            await user.click(increaseButton);
+            await user.click(increaseButton);
+        })
+    
+        expect(addPlantToTotal).toHaveBeenCalledTimes(3);
+      });
+
+      it("calls decreasePlantFromTotal correct number of times", async () => {
+        const user = userEvent.setup();
+
+        const plant = plantFactory('Appletini', 25);
+
+        const addPlantToTotal = jest.fn();
+        const decreasePlantFromTotal = jest.fn();
+        
+        render(<Card 
+            key={plant.name} 
+            plant={plant}
+            addPlant={addPlantToTotal} 
+            decreasePlant={decreasePlantFromTotal}
+        />);
+
+        const decreaseButton = screen.getByRole("button", { name: "-" });
+    
+        await act(async() => {
+            await user.click(decreaseButton);
+            await user.click(decreaseButton);
+            await user.click(decreaseButton);
+        })
+    
+        expect(decreasePlantFromTotal).toHaveBeenCalledTimes(3);
+      });
+
+      it("calls decreasePlantFromTotal correct number of times", async () => {
+        const user = userEvent.setup();
+
+        const plant = plantFactory('Appletini', 25);
+
+        const addPlantToTotal = jest.fn();
+        const decreasePlantFromTotal = jest.fn();
+        
+        render(<Card 
+            key={plant.name} 
+            plant={plant}
+            addPlant={addPlantToTotal} 
+            decreasePlant={decreasePlantFromTotal}
+        />);
+
+        const input = screen.getByRole("spinbutton");
+    
+        await act(async() => {
+            await user.type(input, "126");
+        })
+    
+        expect(screen.getByRole("spinbutton").value).toMatch(/126/i);
+      });
+  });
